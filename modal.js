3f54ce57fd02ea1fa9d05efe689bc27a172e4ab7
DuetoPark <ffdddffddd11@gmail.com>
2020-11-18 00:13:30 +0900

    Javascript: Added modal's background blur effect

--- modal.js
+++ modal.js
@@ -1,45 +1,121 @@
 window.onload = function() {
 
+
+  // Main - Modal Open
   function onClick(){
     document.querySelector('.modal-wrap').style.display='block';
-    document.querySelector('.black-bg').style.display='block';
   }
-
+  // Main - Modal Close
   function closeModal(){
     document.querySelector('.modal-wrap').style.display='none';
-    document.querySelector('.black-bg').style.display='none';
   }
 
-  // document.querySelector('.modal-close').addEventListener('click', closeModal);
   $(".modal-close").click(function() {
     closeModal();
+    document.querySelector('.total').style.filter='none';
   });
-  // document.querySelector('.login-modal-button').addEventListener('click', onClick);
   $(".login-modal-button").click(function() {
     onClick();
+    document.querySelector('.total').style.filter='blur(5px)';
   });
-  // document.querySelector('.signin-modal-button').addEventListener('click', onClick);
   $(".signin-modal-button").off('click').click(function() {
     onClick();
+    document.querySelector('.total').style.filter='blur(5px)';
   });
 
 
-  // My page - Menu
+  // My page - Open Login
   function openMenu(){
-    document.querySelector('.page-menu').style.display='block';
-    document.querySelector('.logout').style.display='block';
+    document.querySelector('.hiddenItems').style.display='block';
   }
+  // My page - Close Login
   function closeMenu(){
-    document.querySelector('.page-menu').style.display='none';
-    document.querySelector('.logout').style.display='none';
+    document.querySelector('.hiddenItems').style.display='none';
   }
 
-  // My page - Menu open
-  $(".menu-button").click(function() {
+  $(".menu-button").off('click').click(function() {
     openMenu();
   });
-  // My page - Menu close
   $(".close-button").off('click').click(function() {
     closeMenu();
+  });
+
+
+
+  // My page - Open Seen Movies
+  let elem = document.querySelector('.reservation-confirm-seen');
+  let pagenation = document.querySelector('.reservation-confirm .col-md-4');
+  let fadeInInterval;
+
+  $('.seen-confirm-button').click(function(){
+
+    clearInterval(fadeInInterval); //동작완료하면 멈추세용.
+
+    elem.fadeIn = function(timing) {
+      let newValue = 0;
+
+      elem.style.display = 'block';
+      elem.style.opacity = 0;
+
+      fadeInInterval = setInterval(function(){
+        if (newValue < 1) {
+          newValue += 0.01;
+        } else if (newValue === 1) {
+          clearInterval(fadeInInterval);
+        }
+
+        elem.style.opacity = newValue;
+
+      }, timing);
+    }
+
+    elem.fadeIn(10);
+    $('.seen-confirm-button').css("display","none");
+  });
+
+
+
+  // My page - Bring More Reviews
+  // Display Reviews
+  let reviews=document.querySelectorAll('.user-review .col-md-4');
+
+  function moreReviews(){
+    for(i=3; i<reviews.length;i++ ){
+      reviews[i].style.display = 'block';
+    }
+    document.querySelector('.more-button').style.display='none';
+  }
+
+  // Fade-in Reviews
+  $('.more-button').off('click').click(function(){
+
+    clearInterval(fadeInInterval); //동작완료하면 멈추세용.
+
+    reviews.fadeIn = function(timing) {
+
+      let newValue = 0;
+
+      for (let i = 3; i < reviews.length; i++) {
+        reviews[i].style.display='block';
+        reviews[i].style.opacity='0';
+      }
+
+      fadeInInterval = setInterval(function(){
+        if (newValue < 1) {
+          newValue += 0.01;
+        } else if (newValue === 1) {
+          clearInterval(fadeInInterval);
+        }
+
+        for (let i = 3; i < reviews.length; i++) {
+          reviews[i].style.opacity = newValue;
+        }
+
+
+      }, timing);
+    }
+    moreReviews();
+    reviews.fadeIn(10);
+
   });
 };
