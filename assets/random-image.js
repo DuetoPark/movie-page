97c144b36611f5c9693711c6261284d769df8f91
DuetoPark <ffdddffddd11@gmail.com>
2020-12-04 10:34:59 +0900

    Javascript: Corrected the misdeclared image src

--- assets/random-image.js
+++ assets/random-image.js
@@ -2,12 +2,12 @@
 // 페이지 로드시, Advertising Section에 이미지가 무작위로 출력.
 $(document).ready(function(){
   let imgArray = new Array();
-  imgArray.push("pictures/록키호러픽쳐쇼2.gif");
-  imgArray.push("pictures/록키호러픽쳐쇼3.gif");
-  imgArray.push("pictures/록키호러픽쳐쇼4.gif");
-  imgArray.push("pictures/록키호러픽쳐쇼5.gif");
-  imgArray.push("./pictures/록키호러픽쳐쇼6.gif");
-  imgArray.push("./pictures/록키호러픽쳐쇼7.gif");
+  imgArray.push("../assets/images/록키호러픽쳐쇼2.gif");
+  imgArray.push("../assets/images/록키호러픽쳐쇼3.gif");
+  imgArray.push("../assets/images/록키호러픽쳐쇼4.gif");
+  imgArray.push("../assets/images/록키호러픽쳐쇼5.gif");
+  imgArray.push("../assets/images/록키호러픽쳐쇼6.gif");
+  imgArray.push("../assets/images/록키호러픽쳐쇼7.gif");
   console.log(imgArray);
   console.log(imgArray.length);
 
