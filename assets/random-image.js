de518f23f1ded434d225a76687581f3e7fc3adfa
DuetoPark <ffdddffddd11@gmail.com>
2021-04-14 15:20:22 +0900

    CLN: 코드 리팩토링 -random-image

--- assets/random-image.js
+++ assets/random-image.js
@@ -1,18 +1,6 @@
-// Main page - Random Image  -> stackoverflow에서 긁어옴.
-// 페이지 로드시, Advertising Section에 이미지가 무작위로 출력.
-$(document).ready(function(){
-  let imgArray = new Array();
-  imgArray.push("../assets/images/록키호러픽쳐쇼2.gif");
-  imgArray.push("../assets/images/록키호러픽쳐쇼3.gif");
-  imgArray.push("../assets/images/록키호러픽쳐쇼4.gif");
-  imgArray.push("../assets/images/록키호러픽쳐쇼5.gif");
-  imgArray.push("../assets/images/록키호러픽쳐쇼6.gif");
-  imgArray.push("../assets/images/록키호러픽쳐쇼7.gif");
-  console.log(imgArray);
-  console.log(imgArray.length);
+// 배너 이미지 랜덤 출력(IE는 동작 안 함ㅠㅠㅠㅠ)
+const banner = document.querySelector("#advertising-banner-image");
+const imagePaths = state.banner.map(image => `../assets/images/${image}.gif`);
+const index = Math.floor(Math.random() * imagePaths.length);
 
-  let imgNum = Math.floor(Math.random() * imgArray.length);
-  let objImg = document.getElementById("advertising-banner-image");
-
-  objImg.src = imgArray[imgNum];
-});
+banner.src = imagePaths[index];
