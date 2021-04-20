d2814c47d2e66a6c4bf0dcfd5e45d8f474fa42d2
DuetoPark <ffdddffddd11@gmail.com>
2021-04-20 18:12:15 +0900

    MOD: 마크업 수정(playing)-cloneNode -01-main & main

--- assets/main.js
+++ assets/main.js
@@ -2,46 +2,63 @@
 let star;
 let genre;
 const prototypeInfo = {
+  displayName: function(data, item) {
+    if (item.dataset.list) return; // playingInfo
+    item.querySelector('.name').textContent = data.name;
+  },
   selectImage: function(data, item) {
-    const path = '../assets/images/' + data.image + '.gif';
-    item.querySelector('.image').setAttribute('src', path);
+    let path;
+    if (item.dataset.list) { // playingInfo
+      path = '../assets/images/' + data.poster + '.jpg';
+    } else { // bannerInfo
+      path = '../assets/images/' + data.image + '.gif';
+    }
 
+    item.querySelector('.image').setAttribute('src', path);
+    item.querySelector('.image').setAttribute('alt', data.name);
   },
   selectStar: function(data, item) {
+    let star = [];
     const fillStar = Math.floor(data.star);
     const emptyStar = Math.ceil(data.star - Math.floor(data.star));
-    star = '★'.repeat(fillStar) + '☆'.repeat(emptyStar) + '(';
+    // star = '★'.repeat(fillStar) + '☆'.repeat(emptyStar) + '(';
 
-    item.querySelector('.icon').textContent = star;
+    for (let i=0; i<fillStar; i+=1) {
+      star = star.concat('★');
+    }
+    for (let i=0; i<emptyStar; i+=1) {
+      star = star.concat('☆');
+    }
+
+    const display = star.join("") + "(";
+    item.querySelector('.icon').textContent = display;
   },
   selectGenre: function(data, item) {
     const keys = Object.keys(data.genre);
     genre = keys
-      .map(function(key) {return this.genre[key] ? key+',' : '';}, data)
+      .map(function(key) {return this.genre[key] ? key : '';}, data)
       .filter(function(item) {return item != ''})
-      .join('')
-      .slice(0, -1);
+      .join(', ');
 
     item.querySelector('.genre > dd').textContent = genre;
   },
   clone: function(data) {
     const item = this.origin.cloneNode(true);
     item.setAttribute('data-origin', false);
-    item.querySelector('.name').textContent = data.name;
-    this.selectImage(data, item);
-
     item.querySelector('.review > p').textContent = data.review;
     item.querySelector('.review > cite').textContent = data.reviewUser;
     item.querySelector('.desc-title').textContent = data.name;
-    this.selectStar(data, item);
     item.querySelector('.rating').setAttribute('aria-label', data.star + '점');
     item.querySelector('.rating').textContent =  data.star;
-    this.selectGenre(data, item);
     item.querySelector('.actor > dd').textContent = data.actor;
     item.querySelector('.director > dd').textContent = data.director;
+
+    this.displayName(data, item);
+    this.selectImage(data, item);
+    this.selectStar(data, item);
+    this.selectGenre(data, item);
     this.container.appendChild(item);
   },
-
 };
 
 function infoFactory(section) {
@@ -53,15 +70,16 @@ function infoFactory(section) {
   return info; // 이걸로 30분 삽질
 }
 
-function populateInfo() {
+function populateInfo(subject) {
   const keys = Object.keys(state.movieList).slice(1);
 
   keys.forEach(function(key) {
-    bannerInfo.clone(state.movieList[key]);
+    subject.clone(state.movieList[key]);
   });
 }
 
 const bannerInfo = infoFactory('#banner');
-// const bannerInfo = infoFactory('#banner');
+const playingInfo = infoFactory('#playing');
 
-populateInfo();
+populateInfo(bannerInfo);
+populateInfo(playingInfo);
