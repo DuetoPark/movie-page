032e016d140309dfa354f91630a05309ccfd0bc1
DuetoPark <ffdddffddd11@gmail.com>
2021-04-25 03:05:55 +0900

    CLN: 파일 삭제(사용 안 함) - random-image

--- assets/random-image.js
+++
@@ -1,6 +0,0 @@
-// 배너 이미지 랜덤 출력(IE는 동작 안 함ㅠㅠㅠㅠ)
-const banner = document.querySelector("#advertising-banner-image");
-const imagePaths = state.banner.map(image => `../assets/images/${image}.gif`);
-const index = Math.floor(Math.random() * imagePaths.length);
-
-banner.src = imagePaths[index];
