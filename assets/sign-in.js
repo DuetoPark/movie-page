172db8b0faa8f26c2d4eb1ef0f315fab761a2ac3
DuetoPark <ffdddffddd11@gmail.com>
2021-04-27 00:42:44 +0900

    DOC: 이벤트 작성 (비밀번호 확인) -sign-in

--- assets/sign-in.js
+++ assets/sign-in.js
@@ -29,3 +29,39 @@ function toggleSignInButton() {
 }
 
 agreePP.addEventListener('change', toggleSignInButton);
+
+
+
+
+// 비밀번호 확인
+const password = document.querySelector('#user-pw');
+const check = document.querySelector('#check-pw');
+let words;
+
+function saveWords() {
+  words = this.value;
+}
+
+function showMessage(state) {
+  const errorMessage = document.querySelector(".error");
+  const okMessage = document.querySelector(".ok");
+
+  if (state === 'ok') {
+    okMessage.classList.remove('hidden');
+    errorMessage.classList.add('hidden');
+  } else if (state === 'error') {
+    errorMessage.classList.remove('hidden');
+    okMessage.classList.add('hidden');
+  }
+}
+
+function checkPassword() {
+  if (this.value === words) { // 일치하면
+    showMessage('ok');
+  } else { // 불일치하면
+    showMessage('error');
+  }
+}
+
+password.addEventListener('keyup', saveWords);
+check.addEventListener('keyup', checkPassword);
