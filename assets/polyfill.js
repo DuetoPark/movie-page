5479b0d7890c97abda292ff669f96d2ab4d34da7
DuetoPark <ffdddffddd11@gmail.com>
2021-05-04 15:47:29 +0900

    DOC: remove 추가 -polyfill

--- assets/polyfill.js
+++ assets/polyfill.js
@@ -7,3 +7,22 @@ if (window.NodeList && !NodeList.prototype.forEach) {
         }
     };
 }
+
+// ChildNode.Remove
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode !== null)
+          this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
