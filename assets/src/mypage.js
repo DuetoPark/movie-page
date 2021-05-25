7be973f5b67b9470e850fe58de0b24d4dc73fc83
DuetoPark <ffdddffddd11@gmail.com>
2021-05-25 15:45:27 +0900

    CLN: 리팩토링(예매내역- 출력) -mypage

--- assets/src/mypage.js
+++ assets/src/mypage.js
@@ -48,86 +48,37 @@ inputFiles.addEventListener('change', readURL);
 
 
 // 예매내역 출력 & pagenation 클릭 이벤트
-// ↓↓↓↓↓↓↓↓↓↓↓↓↓ 여기 리팩토링 하셈 ↓↓↓↓↓↓↓↓↓↓↓↓↓
 const bookedHistory = document.querySelector('#history');
 const historyData = state.booked;
 let currentData = [];
 let currentTableRow = [];
 let currentIndex = 0;
 
-function createHistory() {
-  const table = document.createElement('table');
-  const thead = document.createElement('thead');
-  const tbody = document.createElement('tbody');
+
+function createTr(tbody, fragment) {
   const tr = document.createElement('tr');
-  const headDate = document.createElement('th');
-  const headName = document.createElement('th');
-  const headCount = document.createElement('th');
-  const headSeat = document.createElement('th');
+  tr.innerHTML = `
+    <td class='history-date'></td>
+    <td class='history-name'></td>
+    <td class='history-count'></td>
+    <td class='history-seat'></td>
+  `;
 
-  table.setAttribute('aria-label', '이전 예매 내역');
-  thead.className = 'table-head';
-  headDate.textContent = "날짜";
-  headName.textContent = "영화이름";
-  headCount.textContent = "인원";
-  headSeat.textContent = "좌석";
-  tbody.id = "table-body";
-  tbody.classList = "table-body";
-  tbody.setAttribute('role', 'tabpanel');
-
-  bookedHistory.appendChild(table);
-  table.appendChild(thead);
-  table.appendChild(tbody);
-  thead.appendChild(tr);
-  tr.appendChild(headDate);
-  tr.appendChild(headName);
-  tr.appendChild(headCount);
-  tr.appendChild(headSeat);
-
-  populateCurrentData(tbody);
-}
-
-function populateCurrentData(tbody) {
-  currentData = historyData.slice(currentIndex, currentIndex + 5);
-
-  for (let i=0; i<5; i+=1) {
-    createContent(tbody);
-  }
-  matchData(currentData);
-}
-
-function createContent(tbody) {
-  const tr = document.createElement('tr');
-  const historyDate = document.createElement('td');
-  const historyName = document.createElement('td');
-  const historyCount = document.createElement('td');
-  const historySeat = document.createElement('td');
-
-  historyDate.className = "history-date";
-  historyName.className = "history-name";
-  historyCount.className = "history-count";
-  historySeat.className = "history-seat";
-
-  tbody.appendChild(tr);
-  tr.appendChild(historyDate);
-  tr.appendChild(historyName);
-  tr.appendChild(historyCount);
-  tr.appendChild(historySeat);
-
+  fragment.appendChild(tr);
   currentTableRow.push(tr);
 }
 
 function matchData(currentData) {
-  currentTableRow.forEach(function(tr, index) {
+  currentTableRow.forEach((tr, index) => {
     const dateElement = tr.querySelector(".history-date");
     const nameElement = tr.querySelector(".history-name");
     const countElement = tr.querySelector(".history-count");
     const seatElement = tr.querySelector(".history-seat");
 
-    const dateValue = currentData[index] ? currentData[index].date : "";
-    const nameValue = currentData[index] ? currentData[index].movie.name : "";
-    const countValue = currentData[index] ? currentData[index].count.total : "";
-    const seatValue = currentData[index] ? currentData[index].seat : "";
+    const dateValue = currentData[index] ? currentData[index].date : '';
+    const nameValue = currentData[index] ? currentData[index].movie.name : '';
+    const countValue = currentData[index] ? currentData[index].count.total : '';
+    const seatValue = currentData[index] ? currentData[index].seat : '';
 
     dateElement.textContent = dateValue;
     nameElement.textContent = nameValue;
@@ -136,28 +87,15 @@ function matchData(currentData) {
   });
 }
 
+function populateTbody(tbody) {
+  const fragment = document.createDocumentFragment();
+  for (let i=0; i<5; i+=1) {
+    createTr(tbody, fragment);
+  }
+  tbody.appendChild(fragment);
 
-function createPagenation() {
-  const pagenation = document.createElement('div');
-  const preButton = document.createElement('button');
-  const nextButton = document.createElement('button');
-
-  pagenation.className = "pagenation";
-  pagenation.classList.add('d-flex', 'justify-content-center');
-  preButton.className = "pagenation-button pre inactive";
-  preButton.setAttribute('role', 'tab');
-  preButton.setAttribute('aria-controls', 'table-body');
-  preButton.setAttribute('aria-label', '이전');
-  nextButton.className = "pagenation-button next";
-  nextButton.setAttribute('role', 'tab');
-  nextButton.setAttribute('aria-controls', 'table-body');
-  nextButton.setAttribute('aria-label', '다음');
-
-  bookedHistory.appendChild(pagenation);
-  pagenation.appendChild(preButton);
-  pagenation.appendChild(nextButton);
-
-  pagenation.addEventListener('click', sliceFiveUnits);
+  currentData = historyData.slice(currentIndex, currentIndex + 5);
+  matchData(currentData);
 }
 
 function sliceFiveUnits(e) {
@@ -174,11 +112,46 @@ function sliceFiveUnits(e) {
 
   tbody.innerHTML = "";
   currentTableRow = [];
-  populateCurrentData(tbody);
+  populateTbody(tbody);
 }
 
-createHistory();
-createPagenation();
+(function populateHistory() {
+  bookedHistory.innerHTML = `
+    <table aria-label='이전 예매 내역'>
+      <thead class='table-head'>
+        <tr>
+          <th>날짜</th>
+          <th>영화이름</th>
+          <th>인원</th>
+          <th>좌석</th>
+        </tr>
+      </thead>
+      <tbody id='table-body' class='table-body' role='tabpanel'></tbody>
+    </table>
+  `;
+
+  const tbody = bookedHistory.querySelector('#table-body');
+  populateTbody(tbody);
+})();
+
+(function populatePagenation() {
+  const pagenation = document.createElement('div');
+
+  pagenation.setAttribute('class', 'pagenation d-flex justify-content-center');
+  pagenation.innerHTML = `
+    <button class='pagenation-button pre inactive'
+      role='tab'
+      aria-controls='table-body' aria-label='이전'>
+    </button>
+    <button class='pagenation-button next'
+      role='tab'
+      aria-controls='table-body' aria-label='다음'>
+    </button>
+  `;
+
+  bookedHistory.appendChild(pagenation);
+  pagenation.addEventListener('click', sliceFiveUnits);
+})();
 
 
 
